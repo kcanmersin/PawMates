406bdfc68957367a1353114303dc0d59cd029f63
Kerim Can Mersin <73068663+kcanmersin@users.noreply.github.com>
2024-02-04 00:25:46 +0300

    Initial commit

---
+++ src/main/java/com/PawMates/PawMatesApplication.java
@@ -0,0 +1,13 @@
+package com.PawMates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PawMatesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PawMatesApplication.class, args);
+	}
+
+}
